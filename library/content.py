18f7e9b3f7a23fb6ea6adf468897869e6d95e496
fujimoto <fujimoto@ejpd046-mba.local>
2018-04-26 22:23:11 +0900

    update app

--- library/content.py
+++ library/content.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def app_route():
     return render_template('sample_template.html')
+
+@app.route('/status')
+@app.route('/about')
+def app_status():
+    return render_template('base.html')
