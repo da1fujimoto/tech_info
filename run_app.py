bf6ce93db5785274a9ef91666231d39f260e9f17
D.Fujimoto <da1fujimoto@gmail.com>
2018-04-27 05:09:18 +0000

    apply ssl for local ssl

--- run_app.py
+++ run_app.py
@@ -1,4 +1,5 @@
 import os
+import ssl
 
 from library.content import app
 
@@ -6,4 +7,8 @@ if __name__ == '__main__':
     app.debug = True
     host = os.environ.get('IP', '0.0.0.0')
     port = int(os.environ.get('PORT', 18000))
-    app.run(host=host, port=port)
+
+    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    context.load_cert_chain('cert.crt', 'server_secret.key')
+
+    app.run(host=host, port=port, ssl_context=context)
